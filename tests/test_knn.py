11bb41e9ce562a25279c170b64c14577971cab4a
shotahorii <sh.sinker@gmail.com>
2020-10-09 17:18:13 +0100

    travis

--- tests/test_knn.py
+++ tests/test_knn.py
@@ -1,76 +1,74 @@
-import unittest
 import numpy as np
 import pandas as pd
 from sklearn.datasets import load_boston, load_iris, load_breast_cancer
 from sklearn.neighbors import KNeighborsClassifier, KNeighborsRegressor
 
 import sys
+sys.path.append('./')
 sys.path.append('../')
 
 from machinelfs.supervised.knn import KNNClassifier, KNNRegressor
 from machinelfs.utils.validators import train_test_split
 
-class KNNTest(unittest.TestCase):
+def test_regressor():
+    data = load_boston()
+    X = data.data
+    y = data.target
 
-    def test_regressor(self):
-        data = load_boston()
-        X = data.data
-        y = data.target
+    reg_skl = KNeighborsRegressor(3) #k=3
+    reg_machinelfs = KNNRegressor(3) #k=3
 
-        reg_skl = KNeighborsRegressor(3) #k=3
-        reg_mlfs = KNNRegressor(3) #k=3
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_ratio=0.3, seed=0)
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_ratio=0.3, seed=0)
+    reg_skl.fit(X_train,y_train)
+    reg_machinelfs.fit(X_train,y_train)
 
-        reg_skl.fit(X_train,y_train)
-        reg_machinelfs.fit(X_train,y_train)
+    preds_skl = reg_skl.predict(X_test).tolist()
+    preds_machinelfs = reg_machinelfs.predict(X_test).tolist()
 
-        preds_skl = reg_skl.predict(X_test).tolist()
-        preds_mlfs = reg_machinelfs.predict(X_test).tolist()
+    # should be the same result
+    assert all([a == b for a, b in zip(preds_machinelfs, preds_skl)])
 
-        # should be the same result
-        self.assertListEqual(preds_skl, preds_mlfs)
 
-    def test_classifier_multi(self):
-        data = load_iris()
-        X = data.data
-        y = data.target
+def test_classifier_multi():
+    data = load_iris()
+    X = data.data
+    y = data.target
 
-        clf_skl = KNeighborsClassifier(3) #k=3
-        clf_mlfs = KNNClassifier(3) #k=3
+    clf_skl = KNeighborsClassifier(3) #k=3
+    clf_machinelfs = KNNClassifier(3) #k=3
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_ratio=0.3, seed=0)
-        
-        # one-hot encoding for mlfs classifier
-        y_train_onehot = pd.get_dummies(y_train).values
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_ratio=0.3, seed=0)
+    
+    # one-hot encoding for machinelfs classifier
+    y_train_onehot = pd.get_dummies(y_train).values
 
-        clf_skl.fit(X_train,y_train)
-        clf_machinelfs.fit(X_train,y_train_onehot)
+    clf_skl.fit(X_train,y_train)
+    clf_machinelfs.fit(X_train,y_train_onehot)
 
-        preds_skl = clf_skl.predict(X_test).tolist()
-        # decoding one-hot 
-        preds_mlfs = np.argmax(clf_machinelfs.predict(X_test),axis=1).tolist()
+    preds_skl = clf_skl.predict(X_test).tolist()
+    # decoding one-hot 
+    preds_machinelfs = np.argmax(clf_machinelfs.predict(X_test),axis=1).tolist()
 
-        # should be the same result
-        self.assertListEqual(preds_skl, preds_mlfs)
+    # should be the same result
+    assert all([a == b for a, b in zip(preds_machinelfs, preds_skl)])
 
-    def test_classifier_binary(self):
-        data = load_breast_cancer()
-        X = data.data
-        y = data.target
 
-        clf_skl = KNeighborsClassifier(3) #k=3
-        clf_mlfs = KNNClassifier(3) #k=3
+def test_classifier_binary():
+    data = load_breast_cancer()
+    X = data.data
+    y = data.target
 
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_ratio=0.3, seed=0)
+    clf_skl = KNeighborsClassifier(3) #k=3
+    clf_machinelfs = KNNClassifier(3) #k=3
 
-        clf_skl.fit(X_train,y_train)
-        clf_machinelfs.fit(X_train,y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_ratio=0.3, seed=0)
 
-        preds_skl = clf_skl.predict(X_test).tolist()
-        preds_mlfs = clf_machinelfs.predict(X_test).tolist()
+    clf_skl.fit(X_train,y_train)
+    clf_machinelfs.fit(X_train,y_train)
 
-        # should be the same result
-        self.assertListEqual(preds_skl, preds_mlfs)
+    preds_skl = clf_skl.predict(X_test).tolist()
+    preds_machinelfs = clf_machinelfs.predict(X_test).tolist()
 
-unittest.main()
+    # should be the same result
+    assert all([a == b for a, b in zip(preds_machinelfs, preds_skl)])
--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -1,42 +1,38 @@
-import unittest
 import numpy as np
 from sklearn.datasets import load_breast_cancer
 from sklearn.linear_model import Perceptron as SklPerceptron
 
 import sys
+sys.path.append('./')
 sys.path.append('../')
 
 from machinelfs.supervised.perceptron import Perceptron
 from machinelfs.utils.validators import KFold
 
-class PerceptronTest(unittest.TestCase):
+def test_perceptron():
+    data = load_breast_cancer()
+    X = data.data
+    y = data.target
 
-    def test_sklearn_comparison(self):
-        data = load_breast_cancer()
-        X = data.data
-        y = data.target
+    clf_skl = SklPerceptron()
+    clf_machinelfs = Perceptron(n_epoch=50, seed=1)
 
-        clf_skl = SklPerceptron()
-        clf_mlfs = Perceptron(n_epoch=50, random_seed=1)
+    skl_scores = []
+    machinelfs_scores = []
 
-        skl_scores = []
-        mlfs_scores = []
+    kf = KFold()
+    for train_idx, test_idx in kf.split(X,y):
+        X_train, X_test = X[train_idx], X[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
 
-        kf = KFold()
-        for train_idx, test_idx in kf.split(X,y):
-            X_train, X_test = X[train_idx], X[test_idx]
-            y_train, y_test = y[train_idx], y[test_idx]
+        clf_skl.fit(X_train, y_train)
+        clf_machinelfs.fit(X_train, y_train)
 
-            clf_skl.fit(X_train, y_train)
-            clf_machinelfs.fit(X_train, y_train)
+        skl_scores.append(clf_skl.score(X_test, y_test))
+        machinelfs_scores.append(clf_machinelfs.score(X_test, y_test)['accuracy'])
 
-            skl_scores.append(clf_skl.score(X_test, y_test))
-            mlfs_scores.append(clf_machinelfs.score(X_test, y_test)['accuracy'])
+    skl_score = np.array(skl_scores).mean()
+    machinelfs_score = np.array(machinelfs_scores).mean()
 
-        skl_score = np.array(skl_scores).mean()
-        mlfs_score = np.array(mlfs_scores).mean()
-
-        # accuracy difference from sklearn's Perceptron is less than 5%
-        self.assertTrue(skl_score - mlfs_score < 0.05) 
-
-unittest.main()
+    # accuracy difference from sklearn's Perceptron is less than 5%
+    assert skl_score - machinelfs_score < 0.05
